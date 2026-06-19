1711a819ccf488dc50de20cb843d8fef975128fc
TarunShetty256 <tarunshetty256@gmail.com>
2026-06-19 22:36:18 +0530

    Add Vercel Analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,5 @@
 import type { Metadata } from "next";
+import { Analytics } from "@vercel/analytics/next";
 import { EB_Garamond, Geist, Geist_Mono } from "next/font/google";
 
 import { QUIETLY_ICON_SRC } from "@/lib/brand";
@@ -51,7 +52,10 @@ export default function RootLayout({
       lang="en"
       className={`${geistSans.variable} ${geistMono.variable} ${ebGaramond.variable} h-full antialiased`}
     >
-      <body className="min-h-full flex flex-col">{children}</body>
+      <body className="min-h-full flex flex-col">
+        {children}
+        <Analytics />
+      </body>
     </html>
   );
 }
